8af69f18d35c2c2183c30298ec9117c3725cd556
rahul2409 <rahul_nandrajog@yahoo.com>
2023-07-08 14:53:16 +0530

    First commit - SQL Scripts in .gitignore

---
+++ src/main/java/in/motorglaze/Memberships/MembershipsApplication.java
@@ -0,0 +1,13 @@
+package in.motorglaze.Memberships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembershipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/motorglaze/Memberships/MembershipsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.motorglaze.Memberships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
