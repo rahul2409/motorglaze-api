e9287e61c2dd666d3f06d57a53e084522ade9cf1
rahul2409 <2016.rahul.nandrajog@ves.ac.in>
2023-07-09 19:08:08 +0530

    Added User Entity class and added connection strings and user login values for the DB.

---
+++ app/src/main/java/in/motorglaze/Entity/Users.java
@@ -0,0 +1,107 @@
+package in.motorglaze.Entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+public class Users{
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", columnDefinition = "varchar(50)")
+    private String id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_address")
+    private String email;
+
+    @Column(name = "user_privilege")
+    private String userPrivilege;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+    public Users(){}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Users(String firstName, String lastName, String email, String userPrivilege, String phoneNumber, LocalDateTime createdDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.userPrivilege = userPrivilege;
+        this.phoneNumber = phoneNumber;
+        this.createdDate = createdDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserPrivilege() {
+        return userPrivilege;
+    }
+
+    public void setUserPrivilege(String userPrivilege) {
+        this.userPrivilege = userPrivilege;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", userPrivilege=" + userPrivilege + ", phoneNumber=" + phoneNumber + ", createdDate=" + createdDate + "]";
+    }
+}
